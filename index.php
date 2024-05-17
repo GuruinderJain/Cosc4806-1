bc5cd6ae4a19c1fc86dc8cefc9e9a5412860dfea
GuruinderJain <gujain@algomau.ca>
2024-05-17 03:53:27 +0000

    Added failed attempt, sessions and redirect

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
-   <title>Guruinder</title>
+      <title>Guruinder</title>
   </head>
   <body>
   
   <h1>Assignment1</h1>
     
-  <p><a href="/login.php">Click here to login </a></p>
+    <p> Welcome, <?=$_SESSION['username'] ?></p>
+
+    <p><a href="/login.php">Click here to login </a></p>
 </body>
 </html>
